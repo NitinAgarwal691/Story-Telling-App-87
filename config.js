0e34d2410a527b520edccee7ad60ebf5fa0d5d7d
NitinAgarwal691 <73051793+NitinAgarwal691@users.noreply.github.com>
2021-09-11 17:02:42 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBUA11rTDdTV0AzPkeH38AdIFWnoBkODFU",
+  authDomain: "story-telling-app-c1750.firebaseapp.com",
+  databaseURL: "https://story-telling-app-c1750-default-rtdb.firebaseio.com",
+  projectId: "story-telling-app-c1750",
+  storageBucket: "story-telling-app-c1750.appspot.com",
+  messagingSenderId: "202490771694",
+  appId: "1:202490771694:web:26d567a0c8b8e57019dbff"
+};
